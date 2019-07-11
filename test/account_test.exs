c6a6841a3a1ad211b065007bd79f42ef5148d7e9
Andy Skelton <skeltoac@gmail.com>
2019-07-11 15:53:46 -0500

    Remove unnecessary quotes from atoms

--- test/account_test.exs
+++ test/account_test.exs
@@ -11,19 +11,19 @@ defmodule Shipstation.AccountTest do
     use_cassette "register_user" do
       params =
         %Shipstation.Structs.Account{
-          "firstName":                 "John",
-          "lastName":                  "Smith",
-          "email":                     "jsmithtest@gmail.com",
-          "password":                  "testpw1234",
-          "shippingOriginCountryCode": "US",
-          "companyName":               "Droid Repair LLC",
-          "addr1":                     "542 Midichlorian Rd.",
-          "addr2":                     "",
-          "city":                      "Austin",
-          "state":                     "TX",
-          "zip":                       "78703",
-          "countryCode":               "US",
-          "phone":                     "5124111234"
+          firstName:                 "John",
+          lastName:                  "Smith",
+          email:                     "jsmithtest@gmail.com",
+          password:                  "testpw1234",
+          shippingOriginCountryCode: "US",
+          companyName:               "Droid Repair LLC",
+          addr1:                     "542 Midichlorian Rd.",
+          addr2:                     "",
+          city:                      "Austin",
+          state:                     "TX",
+          zip:                       "78703",
+          countryCode:               "US",
+          phone:                     "5124111234"
         }
 
       assert {:ok, %{status_code: 200}} = Shipstation.Account.register(params)
